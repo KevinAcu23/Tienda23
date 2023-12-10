38d89315f530e63c86a161d1fe939ea731fa0282
KevinAcu23 <acunajaenkevin@gmail.com>
2023-12-09 23:03:37 -0600

    Semana 13

--- Tienda_l/src/main/java/com/tienda/domain/Usuario.java
+++ Tienda_l/src/main/java/com/tienda/domain/Usuario.java
@@ -7,37 +7,27 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 
 public class Usuario implements Serializable {
-    
-    private static final long serialVersionUID=1l;
-    
+
+    private static final long serialVersionUID = 1l;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_usuario")
-    private Long idCategoria;
+    @Column(name = "id_usuario")
+    private Long idUsuario;
     private String username;
     private String password;
     private String nombre;
     private String apellidos;
-    private String telefono;
     private String correo;
+    private String telefono;
     private String rutaImagen;
     private boolean activo;
-    
+
     @OneToMany
-    @JoinColumn(name="id_usuario",updatable=false)
+    @JoinColumn(name = "id_usuario", updatable = false)
     List<Rol> roles;
-
-    public Long getIdUsuario() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
-
-   
-    }
-
-    
-    
+}
 
